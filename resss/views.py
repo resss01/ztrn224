915d0bde1c037a093f9e83953c98617a1bb7c0d2
resss01 <resulsuvhanov@gmail.com>
2025-06-15 21:24:43 -0400

    commit

--- resss/views.py
+++ resss/views.py
@@ -3,12 +3,12 @@ from .models import FormSubmission
 
 def submit_form(request):
     if request.method == 'POST':
-        data = {key: value for key, value in request.POST.items() if key.startwith('name')}
-        FormSubmission.objects.crate(data=data)
+        data = {key: value for key, value in request.POST.items() if key.startswith('name')}
+        FormSubmission.objects.create(data=data)
         return redirect('view_data')
     return render(request, 'resss/resss.html')
     
 def view_data(request):
-    submissions = FormSubmission.object.all()
-    return render(request, 'resss/resss.html', {'submissions': submissions})
+    submissions = FormSubmission.objects.all()
+    return render(request, 'resss/data.html', {'submissions': submissions})
     
